183eb75ee3bf562a76ac61b936992b4bf4f27aba
Oleg Romanov <olegromanov.3003@gmail.com>
2024-07-17 13:27:48 +0300

    Init project

---
+++ RickAndMortyApp/RickAndMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyApp
+//
+//  Created by Олег Романов on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Олег Романов on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
