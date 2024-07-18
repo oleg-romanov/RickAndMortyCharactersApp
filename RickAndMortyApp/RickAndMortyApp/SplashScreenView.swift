1b23e4ce6035930a375727c1ffa18417f8df1afb
Oleg Romanov <olegromanov.3003@gmail.com>
2024-07-18 23:45:13 +0300

    Implement splash screen

---
+++ RickAndMortyApp/RickAndMortyApp/SplashScreenView.swift
@@ -0,0 +1,52 @@
+//
+//  SplashScreenView.swift
+//  RickAndMortyApp
+//
+//  Created by Олег Романов on 18.07.2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State private var isActive = false
+    @State private var size: CGFloat = 1.0
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Image("RickAndMortyBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
+                Image("RickAndMortyLogo")
+                    .padding(.bottom, 16)
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(Animation.easeIn(duration: 1.2).repeatForever(autoreverses: true)) {
+                            self.size = 1.2
+                            self.opacity = 1.0
+                        }
+                    }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    SplashScreenView()
+}
